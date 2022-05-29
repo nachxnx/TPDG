d0b63f1b3d30c864fde9ec1ac15dcef6706df569
nachxnx <nachxnx@gmail.com>
2022-05-29 17:10:57 -0400

    dele

--- data/download_pix2pix_dataset.sh
+++
@@ -1,8 +0,0 @@
-FILE=$1
-URL=http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/$FILE.tar.gz
-TAR_FILE=./$FILE.tar.gz
-TARGET_DIR=./$FILE/
-wget -N $URL -O $TAR_FILE
-mkdir $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./
-rm $TAR_FILE
